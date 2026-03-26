39e3e01dea948823d728cacf0b97f6e46527b53d
ross0778 <ross0778@umn.edu>
2026-03-26 14:46:01 -0500

    Posts from past month

--- R/reddit_api.R
+++ R/reddit_api.R
@@ -1,7 +1,14 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
-library(httr)
+library(RedditExtractoR)
 library(jsonlite)
 
 # Data Import and Cleaning
+urls_r <- find_thread_urls(
+  subreddit = "rstats",
+  sort_by = "new",
+  period = "month"
+)
+
+  
